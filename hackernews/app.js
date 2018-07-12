636424c06b65d0204f0362f085c3dd8ed74604f3
Doyin Olarewaju <youngenterpreneur11@gmail.com>
2018-07-12 17:45:05 +0100

    Initial Commit

--- hackernews/app.js
+++ hackernews/app.js
@@ -1,6 +1,7 @@
 (function (){
     'use strict';
 
+    // General app object that holds constructor data
     var app = {
         axiosObject: axios.create({
             baseURL:'https://hacker-news.firebaseio.com/v0/',
@@ -17,10 +18,12 @@
     // the template for each news item
     app.templateContainer.classList.add('media')
 
+    //fetch the initial news items
     app.fetchNews = function () {
         app.axiosObject.get('topstories.json?print=pretty')
                        .then(handleSuccessData, handleError)
-        
+
+        //handle the success of the request
         function handleSuccessData (s) {
             if(s.status === 200){
                 app.dataStore = s.data
@@ -32,12 +35,14 @@
             //Pass the error to handleError so we have a common way of handling errors
         }
 
+        // handle the error response
         function handleError (e) {
             alert('An error occurred')
             //we can log the error here and also display a message
         } 
     }
 
+    //This function handles sorting the news items to show based on the current page
     function handleNewsItemsDisplay () {
         var news = app.dataStore.slice(app.page, app.currentItemsCount)
         var newsItems = news.map(prepareData)
@@ -50,15 +55,19 @@
         app.currentItemsCount += app.limit
     }
 
-    function prepareData (data, id) {
+    //prepare the data by placing it in a template
+    function prepareData (data) {
+        // this is the main template container, its just a div
         var templateCopy = app.templateContainer.cloneNode()
         var templateBody = document.createElement('div')
         templateBody.classList.add('media-body')
 
+        // the template heading
         var heading = document.createElement('h4')
         heading.classList.add('media-heading')
         heading.textContent = `Some Title`
 
+        // the text inside the template body itself
         var text = document.createElement('p')
         text.textContent = data
 
@@ -69,6 +78,7 @@
         return templateCopy
     }
 
+    // we handle the infinite scrolling here by watching when scroll event is at the bottom of the page
     document.onscroll = function() {
         if(document.documentElement.scrollTop + window.innerHeight == document.documentElement.scrollHeight)
         {
@@ -76,6 +86,7 @@
         }
     }
 
+    //after data has been fetched hide the loading text.
     function hideMainLoader () {
         app.mainLoader.style.visibility = 'hidden'
     }
